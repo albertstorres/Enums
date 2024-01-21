883d36a20a390aca1bcf71441db6ccaac829a829
Albert Sevy <albertstorres@gmail.com>
2024-01-21 02:11:55 -0300

    Finalizando o repositório da aula de enumeradores.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Enums
     {
         static void Main(string[] args)
         {
-            
+            var cliente = new Cliente("Joáo Silva", EEstadoCivil.Casado);
+
+            Console.WriteLine(cliente.Nome);
+            Console.WriteLine(cliente.EstadoCivil);
+            Console.WriteLine((int)cliente.EstadoCivil);
+        }
+    }
+
+    enum EEstadoCivil
+    {
+        Solteiro = 1,
+        Casado = 2,
+        Divorciado = 3
+    }
+
+    struct Cliente
+    {
+        public string Nome;
+        public EEstadoCivil EstadoCivil;
+
+        public Cliente(string nome, EEstadoCivil estadoCivil)
+        {
+            Nome = nome;
+            EstadoCivil = estadoCivil;
         }
     }
 }
